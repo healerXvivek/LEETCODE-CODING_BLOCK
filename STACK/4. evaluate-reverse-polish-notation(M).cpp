e289b1fb44b9979e00d592c8b020746db962652b
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-04-18 12:23:23 +0530

    Create 4. evaluate-reverse-polish-notation(M).cpp
    https://leetcode.com/problems/evaluate-reverse-polish-notation/description/

---
+++ STACK/4. evaluate-reverse-polish-notation(M).cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>s;
+        for(auto x:tokens)
+        {
+            if(x=="+" || x=="-" || x=="*" || x=="/")
+            {
+                int n=s.top(); s.pop();
+                int m=s.top(); s.pop();
+
+                if(x=="+")
+                s.push(n+m);
+
+                if(x=="-")
+                s.push(m-n);
+
+                if(x=="*")
+                s.push(m*n);
+
+                if(x=="/")
+                s.push(m/n);
+            }
+            else{
+             s.push(stol(x));
+            }
+        }
+        return (int)s.top();
+    }
+};
