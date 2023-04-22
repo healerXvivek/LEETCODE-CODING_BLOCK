95133610fcc7873ba4787faac96dc9b6184efef3
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-04-22 20:27:06 +0530

    Create 13. set-mismatch(E).cpp
    https://leetcode.com/problems/set-mismatch/description/

---
+++ HASHING/13. set-mismatch(E).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& arr) {
+    int i=0;
+    int n=arr.size();
+    while(i<n){
+        int correct=arr[i]-1;
+        if(arr[i]!=arr[correct])
+        swap(arr[i],arr[correct]);
+        else
+        i++;
+    }
+    for(int i=0;i<n;i++){ 
+        if(arr[i]!=i+1)
+        return {arr[i],i+1};
+    }
+    return {0};
+    }
+};
