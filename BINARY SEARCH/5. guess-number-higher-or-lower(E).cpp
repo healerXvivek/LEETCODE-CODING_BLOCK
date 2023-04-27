dab38ef0ff35ea5b8f441b81e60dcf044e1beb93
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-04-27 15:23:52 +0530

    Create 5. guess-number-higher-or-lower(E).cpp
    https://leetcode.com/problems/guess-number-higher-or-lower/

---
+++ BINARY SEARCH/5. guess-number-higher-or-lower(E).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1; int h= n;
+        while(l <= h){
+            int mid = l+(h-l)/2;
+            if(guess(mid) == 0) return mid;
+            else if(guess(mid)== -1)
+                h = mid-1;
+            else if(guess(mid) == 1)
+                l = mid+1;
+            
+        }
+        return 0;
+    }
+};
