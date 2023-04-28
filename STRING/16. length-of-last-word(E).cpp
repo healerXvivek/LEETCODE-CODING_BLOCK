4712dfc64df756332d1090266d354334e10b1930
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-04-28 22:29:13 +0530

    Create 16. length-of-last-word(E).cpp
    https://leetcode.com/problems/length-of-last-word/description/

---
+++ STRING/16. length-of-last-word(E).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.length();
+        int i=n-1;
+        while(s[i]==' ')
+        i--;
+        int cnt=0;
+        while(i>=0 && s[i]!=' '){
+            cnt++;
+            i--;
+        }
+        return cnt;
+    }
+};
